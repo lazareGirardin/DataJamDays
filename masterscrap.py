4e884af9a4b01ba95d9484c5040abbe28c82b2ed
Lazare <lazare.girardin@epfl.ch>
2017-11-25 12:18:28 +0100

    Louis screwd up once again, here the good version

--- masterscrap.py
+++ masterscrap.py
@@ -5,26 +5,26 @@ def masterscrap(conseil_dic , nb_pages):
 	maxIdx = nb_pages
 
 	for index, (k, v) in enumerate(conseil_dict.items()):
-    	pageIdx = 1
-    	receiving = True
-    	counselor_votes = []
-    	while(receiving):
-        	_vote = fetch_councillor(v, pageIdx)
-        	print (pageIdx)
-        	if (_vote.status_code != requests.codes.ok or pageIdx >= maxIdx):
-            	receiving = False
-        	else:
-            	counselor_votes.append(_vote)
-            	pageIdx+=1
-    	votes.append(counselor_votes)
-    	print(votes)
+		pageIdx = 1
+		receiving = True
+		counselor_votes = []
+		while(receiving):
+			_vote = fetch_councillor(v, pageIdx)
+			print (pageIdx)
+			if (_vote.status_code != requests.codes.ok or pageIdx >= maxIdx):
+				receiving = False
+			else:
+				counselor_votes.append(_vote)
+				pageIdx+=1
+		votes.append(counselor_votes)
+		print(votes)
 
 
 	df = pd.DataFrame()
 	for counselor in votes:
-    	for vote in counselor:
-        	j = vote.json()
-        	for eachvote in j['affairVotes']:
-            	df.loc[eachvote['id'], j['id']] = eachvote['councillorVote']['decision']
+		for vote in counselor:
+			j = vote.json()
+			for eachvote in j['affairVotes']:
+				df.loc[eachvote['id'], j['id']] = eachvote['councillorVote']['decision']
 
-    return(df)
+	return(df)
