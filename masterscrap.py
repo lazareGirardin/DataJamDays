f69a4a986bce8c3f395e2c148913055247e1f29f
Lazare <lazare.girardin@epfl.ch>
2017-11-25 12:25:17 +0100

    I am getting tired of correcting Louis' mistakes

--- masterscrap.py
+++ masterscrap.py
@@ -1,3 +1,19 @@
+import requests
+import json
+import pandas as pd
+
+def fetch_councillor(id_, page):
+	url = "http://ws-old.parlament.ch/votes/councillors/" + str(id_)
+	print(url)
+	params = {
+		'format':'json',
+		'pageNumber': page}
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+	}
+	resp = requests.get(url=url, params=params, headers=headers)
+	return resp
+
 def masterscrap(conseil_dict , nb_pages):
 	"return a beautiful df. Also Lazare coded 3 nested for loops, cheers to that."
 	
